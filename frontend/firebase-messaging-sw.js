d335c29a951c1e9c31d74c1717b90dfe2d8b693a
Srusti M <srustishettym@gmail.com>
2026-03-17 12:04:30 +0530

    Update firebase-messaging-sw.js

--- frontend/firebase-messaging-sw.js
+++ frontend/firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDGk5u59oxUqrG0ZfA2k1ONo9M_HI3wtrU",
-  authDomain: "smartnoticeboard-fb51b.firebaseapp.com",
-  projectId: "smartnoticeboard-fb51b",
-  storageBucket: "smartnoticeboard-fb51b.firebasestorage.app",
-  messagingSenderId: "1050872240810",
-  appId: "1:1050872240810:web:9aeacf3e3cc5cf0c464ed3"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 });
 
 const messaging = firebase.messaging();
